6044178a57e79c2debc523653ba151da9d523e58
glozanoa <glozanoa@uni.pe>
2022-07-29 00:10:15 -0500

    update PodController and PodManager

--- src/main/java/org/example/multipods/PodController.java
+++ src/main/java/org/example/multipods/PodController.java
@@ -1,7 +1,17 @@
-package org.example;
+package org.example.multipods;
 
 public class PodController {
   private Pod pod;
 
-  
+  public PodController(Pod pod) {
+    this.pod = pod;
+  }
+
+  public void run() {
+    pod.run();
+  }
+
+  public void exec(String cmd) {
+    pod.exec(cmd);
+  }
 }
--- src/main/java/org/example/multipods/PodManager.java
+++ src/main/java/org/example/multipods/PodManager.java
@@ -1,27 +1,29 @@
-package org.example;
+package org.example.multipods;
 
-import java.awt.*;
 import java.util.HashMap;
+import java.util.Map;
 
 public class PodManager {
   private static Map<String, PodController> controllers;
   private static PodManager instance = null;
 
-  private PodManager(){
+  private PodManager() {
     controllers = new HashMap<String, PodController>();
     instance = this;
   }
 
-  public PodManager getInstance(){
-    if(instance == null){
+  public PodManager getInstance() {
+    if(instance == null) {
       instance = new PodManager();
     }
     return instance;
   }
 
   public String createController(ContainerFactory factory, String image, String tag){
-
+    //PodImage podImage = factory.
   }
 
-  public void exec()
+  public void exec(String podId, String cmd){
+    // search pod by podId and execute cmd
+  }
 }
