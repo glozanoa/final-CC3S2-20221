2ebe55cd7b334d823a4fe39a601da2d3a552c40e
hpord31 <hannu.portillo.d@uni.pe>
2022-07-29 00:16:29 -0500

    cambiando tabulacion

--- src/main/java/org/example/multipods/DockerPod.java
+++ src/main/java/org/example/multipods/DockerPod.java
@@ -1,12 +1,12 @@
 package org.example.multipods;
 
 public class DockerPod extends Pod{
-    public DockerPod() {
-        ;
-    }
+  public DockerPod() {
+    ;
+  }
 
-    @Override
-    public void run() {
-        System.out.println("Run DockerPod");
-    }
+  @Override
+  public void run() {
+    System.out.println("Run DockerPod");
+  }
 }
--- src/main/java/org/example/multipods/Pod.java
+++ src/main/java/org/example/multipods/Pod.java
@@ -2,16 +2,16 @@ package org.example.multipods;
 
 public abstract class Pod {
 
-    public Pod() {
-        ;
-    }
-    public abstract void run();
+  public Pod() {
+    ;
+  }
+  public abstract void run();
 
-    public void exec(String cmd) {
-        ;
-    }
+  public void exec(String cmd) {
+    ;
+  }
 
-    public void pullPodImage() {
-        ;
-    }
+  public void pullPodImage() {
+    ;
+  }
 }
