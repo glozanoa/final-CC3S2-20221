ac62d0e4d935b4b2a2c11b73894fee64fc547d6f
hpord31 <hannu.portillo.d@uni.pe>
2022-07-28 23:47:09 -0500

    creacion de clases DockerPod y PodmanPod

--- src/main/java/org/example/multipods/Pod.java
+++ src/main/java/org/example/multipods/Pod.java
@@ -1,3 +1,17 @@
+package org.example.multipods;
+
 public abstract class Pod {
+
+    public Pod() {
+        ;
+    }
     public abstract void run();
+
+    public void exec(String cmd) {
+        ;
+    }
+
+    public void pullPodImage() {
+        ;
+    }
 }
