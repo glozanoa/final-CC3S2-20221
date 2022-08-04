2f5e6c44435c80ca5189c6a171fc935fe24f9c41
glozanoa <glozanoa@uni.pe>
2022-08-04 06:28:36 -0500

    update command parser

---
+++ src/main/java/org/example/multipods/ContainerTechnology.java
@@ -0,0 +1,6 @@
+package org.example.multipods;
+
+public enum ContainerTechnology {
+    DOCKER,
+    PODMAN
+}
--- src/main/java/org/example/multipods/Multipod.java
+++ src/main/java/org/example/multipods/Multipod.java
@@ -7,6 +7,13 @@ import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 import net.sourceforge.argparse4j.inf.Subparser;
 import net.sourceforge.argparse4j.inf.Subparsers;
+import org.example.multipods.docker.DockerFactory;
+import org.example.multipods.podman.PodmanFactory;
+
+enum Command {
+  PULL,
+  RUN
+}
 
 public class Multipod {
   protected static String image;
@@ -15,7 +22,42 @@ public class Multipod {
   public static void main(String[] args) {
     ArgumentParser parser = commandParser();
     try {
-      parseCmdArgs(parser, args);
+      Namespace ns = parser.parseArgs(args);
+
+      System.out.println(ns);
+
+      Command command = ns.get("command");
+      String imageName = ns.getString("image");
+      String imageTag = ns.getString("tag");
+      ContainerTechnology technology = ns.get("technology");
+
+      ContainerFactory factory;
+
+      // factory selector
+      switch (technology){
+        case PODMAN:
+          factory = new PodmanFactory();
+          break;
+
+        default:
+          factory = new DockerFactory();
+          break;
+      }
+
+      PodManager manager = PodManager.getInstance();
+
+      // command selector
+      switch (command){
+        case RUN:
+          manager.runController(factory, imageName, imageTag);
+          break;
+
+        case PULL:
+          manager.pullPodImage(factory, imageName, imageTag);
+          break;
+      }
+
+
     } catch (ArgumentParserException error) {
       System.err.println(error);
     }
@@ -27,27 +69,46 @@ public class Multipod {
             .defaultHelp(true)
             .description("Multipod - Command Line Options");
 
-    Subparsers subparsers = parser.addSubparsers().help("sub-command help");
-    Subparser parserRun = subparsers.addParser("run").help("Use this command to run a pod image");
-    parserRun.addArgument("image").type(String.class)
-            .help("Enter Name and Tag of the image to run (Example: mongo:10.0)");
-    Subparser parserPull = subparsers.addParser("pull")
-            .help("Use this command to pull a pod image");
-    parserPull.addArgument("image").type(String.class)
-            .help("Enter Name and Tag of the image to pull (Example: mongo:10.0)");
-    parser.addArgument("--technology")
+    Subparsers subparsers = parser.addSubparsers()
+            .help("sub-command help");
+
+    // run subparser
+
+    Subparser parserRun = subparsers
+            .addParser("run")
+            .setDefault("command", Command.RUN)
+            .help("Use this command to run a pod image");
+
+    parserRun.addArgument("image")
             .type(String.class)
-            .help("Technology to use for virtualization of the image (Docker or Podman)");
+            .help("Image name");
+    parserRun.addArgument("tag")
+            .type(String.class)
+            .setDefault("latest")
+            .required(false)
+            .help("Image tag");
+
+    // pull subparser
+
+    Subparser parserPull = subparsers.addParser("pull")
+            .setDefault("command", Command.PULL)
+            .help("Use this command to pull a pod image");
+
+    parserPull.addArgument("image")
+            .type(String.class)
+            .help("Image name");
+    parserPull.addArgument("tag")
+            .type(String.class)
+            .setDefault("latest")
+            .required(false)
+            .help("Image tag");
+
+
+    parser.addArgument("-T", "--technology")
+            .type(ContainerTechnology.class)
+            .choices(ContainerTechnology.values()).setDefault(ContainerTechnology.DOCKER)
+            .help("Container technology to use for virtualization of the image.");
 
     return parser;
-  }
-
-  public static void parseCmdArgs(ArgumentParser parser, String[] args)
-          throws ArgumentParserException {
-    Namespace ns = parser.parseArgs(args);
-    //parser.
-    //image = ns.getString("run");
-    //image = ns.getString("pull");
-    //tech = ns.getString("technology");
   }
 }
