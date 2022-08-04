a420ef62b90fa7434e803c730bd51d39fce153ae
glozanoa <glozanoa@uni.pe>
2022-08-04 00:23:02 -0500

    add multipod tasks

--- src/main/java/org/example/multipods/Multipod.java
+++ src/main/java/org/example/multipods/Multipod.java
@@ -8,19 +8,22 @@ public class Multipod {
   protected static String image;
   protected static String tech;
 
-  public static void main(String[] args) throws ArgumentParserException {
+  public static void main(String[] args){
     ArgumentParser parser = commandParser();
-    parseCmdArgs(parser, args);
+    try {
+      parseCmdArgs(parser, args);
+    }
+    catch (ArgumentParserException error){
+      System.err.println(error);
+    }
+
   }
 
   public static ArgumentParser commandParser() {
     ArgumentParser parser = ArgumentParsers.newFor("Multipod").build()
             .defaultHelp(true)
             .description("Multipod - Command Line Options");
-    parser.addArgument("multipod")
-            .action(Arguments.storeTrue())
-            .type(String.class)
-            .help("Use for enable multipod program");
+
     Subparsers subparsers = parser.addSubparsers().help("sub-command help");
     Subparser parserRun = subparsers.addParser("run").help("Use this command to run a pod image");
     parserRun.addArgument("image").type(String.class)
@@ -37,8 +40,9 @@ public class Multipod {
 
   public static void parseCmdArgs(ArgumentParser parser, String[] args) throws ArgumentParserException {
     Namespace ns = parser.parseArgs(args);
-    image = ns.getString("run");
-    image = ns.getString("pull");
-    tech = ns.getString("technology");
+    //parser.
+    //image = ns.getString("run");
+    //image = ns.getString("pull");
+    //tech = ns.getString("technology");
   }
 }
